242e2196b19be675ca2f9f1fde01bb70fc1fc75e
edwilson <edwilsonfa@gmail.com>
2023-11-27 16:33:50 -0300

    Project created

---
+++ src/main/java/com/api/money/apimoney/ApiMoneyApplication.java
@@ -0,0 +1,13 @@
+package com.api.money.apimoney;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiMoneyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiMoneyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/money/apimoney/ApiMoneyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.money.apimoney;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiMoneyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
