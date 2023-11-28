97e2eeac43d4b3c0bb97dbacfacb914a5acb14f4
Ed Wilson <edwilsonfa@gmail.com>
2023-11-27 22:53:29 -0300

    Implementation Get/Post (Categoria)

--- src/main/java/com/api/money/apimoney/resource/CategoriaResource.java
+++ src/main/java/com/api/money/apimoney/resource/CategoriaResource.java
@@ -2,11 +2,16 @@ package com.api.money.apimoney.resource;
 
 import com.api.money.apimoney.model.Categoria;
 import com.api.money.apimoney.repository.CategoriaRepository;
+import jakarta.persistence.Entity;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -16,7 +21,25 @@ public class CategoriaResource {
     private CategoriaRepository categoriaRepository;
 
     @GetMapping
-    public List<Categoria> listar(){
+    public List<Categoria> listar() {
         return categoriaRepository.findAll();
     }
+
+    @PostMapping
+    //@ResponseStatus(HttpStatus.CREATED)
+    public ResponseEntity<Categoria> criar(@RequestBody Categoria categoria, HttpServletResponse response) {
+        Categoria categoriaSalva = categoriaRepository.save(categoria);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{codigo}").buildAndExpand(categoriaSalva.getCodigo()).toUri();
+        response.setHeader("Location", uri.toASCIIString());
+        return ResponseEntity.created(uri).body(categoriaSalva);
+
+    }
+
+    @GetMapping("/{codigo}")
+    public Categoria buscarPeloCodigo(@PathVariable Long codigo) {
+        return categoriaRepository.findById(codigo).get();
+    }
+
+
 }
+
