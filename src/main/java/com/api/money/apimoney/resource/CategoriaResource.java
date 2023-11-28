4c11210ff482dd99a5ab888a1e57ca466b9a1f57
edwilson <edwilsonfa@gmail.com>
2023-11-28 15:43:41 -0300

    Created exception class and file messages.properties

--- src/main/java/com/api/money/apimoney/resource/CategoriaResource.java
+++ src/main/java/com/api/money/apimoney/resource/CategoriaResource.java
@@ -3,6 +3,7 @@ package com.api.money.apimoney.resource;
 import com.api.money.apimoney.model.Categoria;
 import com.api.money.apimoney.repository.CategoriaRepository;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EntityNotFoundException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,6 +14,7 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/categorias")
@@ -20,9 +22,19 @@ public class CategoriaResource {
     @Autowired
     private CategoriaRepository categoriaRepository;
 
+//    @GetMapping
+//    public ResponseEntity<?> listar(){
+//        List<Categoria> categorias = categoriaRepository.findAll();
+//        return !categorias.isEmpty() ? ResponseEntity.ok(categorias) : ResponseEntity.notFound().build();
+//    }
+
     @GetMapping
     public List<Categoria> listar() {
         return categoriaRepository.findAll();
+    }
+    @GetMapping("/{codigo}")
+    public Categoria buscarPeloCodigo(@PathVariable Long codigo) {
+        return categoriaRepository.findById(codigo).get();
     }
 
     @PostMapping
@@ -35,10 +47,6 @@ public class CategoriaResource {
 
     }
 
-    @GetMapping("/{codigo}")
-    public Categoria buscarPeloCodigo(@PathVariable Long codigo) {
-        return categoriaRepository.findById(codigo).get();
-    }
 
 
 }
